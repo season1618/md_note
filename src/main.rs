933153368798f15b93fec7b3ebddd5784917a93a
season1618 <seasonminority314@gmail.com>
2023-01-19 19:35:00 +0900

    render all as paragraph.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,11 @@ use std::io::{BufRead, BufReader, Write};
 use std::env;
 use std::fs::File;
 
+// macro_rules! writeln {
+//     // ( $ dst : expr , $ ( $ arg : tt ) * ) => { ... };
+//     ( $arg1: tt, $arg2: tt ) => { write!($arg1, $arg2, "") };
+// }
+
 fn main(){
     let args: Vec<String> = env::args().collect();
     let file_path = &args[1];
@@ -19,16 +24,39 @@ impl Convertor {
     fn new() -> Self {
         Convertor { output_file: File::create("test.html").unwrap() }
     }
+
     fn convert(&mut self, file_path: &str) {
         match File::open(file_path) {
             Ok(input_file) => {
+                write!(self.output_file, "<!DOCTYPE html>\n");
+                write!(self.output_file, "<html>\n");
+                write!(self.output_file, "<head>\n");
+                write!(self.output_file, "  <meta charset=\"utf-8\">\n");
+                write!(self.output_file, "  <link rel=\"stylesheet\" href=\"./index.css\">\n");
+                write!(self.output_file, "  <title></title>\n");
+                write!(self.output_file, "</head>\n");
+                write!(self.output_file, "<body>\n");
+                
+                write!(self.output_file, "  <div id=\"wrapper\">\n");
+
+                write!(self.output_file, "    <nav id=\"sidebar\">\n");
+                write!(self.output_file, "    </nav>\n");
+
+                write!(self.output_file, "    <div id=\"content\">\n");
+
                 for result in BufReader::new(input_file).lines() {
                     if let Ok(s) = result {
                         self.convert_line(s);
                     } else {
                         println!("could not read a line.\n");
+                        break;
                     }
                 }
+
+                write!(self.output_file, "    <div>\n");
+
+                write!(self.output_file, "</body>\n");
+                write!(self.output_file, "</html>\n");
                 
             },
             Err(err) => {
@@ -38,7 +66,6 @@ impl Convertor {
     }
 
     fn convert_line(&mut self, s: String) {
-        self.output_file.write_all(s.as_bytes()).unwrap();
-        self.output_file.write_all(b"\n").unwrap();
+        write!(self.output_file, "      <p>{}</p>\n", s);
     }
 }
