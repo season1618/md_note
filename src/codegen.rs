d0c86d2547be1a2b0032c21b2a24b6753610e7c0
season1618 <seasonminority314@gmail.com>
2023-12-24 22:25:56 +0900

    adjust indent.

--- src/codegen.rs
+++ src/codegen.rs
@@ -13,46 +13,46 @@ use Elem::*;
 pub fn gen_html(dest: &mut File, title: &String, toc: &List, content: &Vec<Block>, template: &Vec<Elem>) -> Result<(), io::Error> {
     for line in template {
         match line {
-            Title(indent) => { writeln!(dest, "  <title>{}</title>", title)?; },
-            Toc(indent) => { gen_sidebar(dest, title, toc)?; },
-            Content(indent) => { gen_content(dest, content)?; },
+            Title(ref indent) => { writeln!(dest, "{:>indent$}<title>{}</title>", " ", title)?; },
+            Toc(indent) => { gen_sidebar(dest, title, toc, *indent)?; },
+            Content(indent) => { gen_content(dest, content, *indent)?; },
             Str(text) => { write!(dest, "{}", text)?; },
         }
     }
     Ok(())
 }
 
-fn gen_sidebar(dest: &mut File, title: &String, toc: &List) -> Result<(), io::Error> {
-    writeln!(dest, "    <nav id=\"toc\">")?;
-    writeln!(dest, "      <h4>{}</h4>", title)?;
-    gen_list(&toc, 6, dest)?;
-    writeln!(dest, "    </nav>")
+fn gen_sidebar(dest: &mut File, title: &String, toc: &List, indent: usize) -> Result<(), io::Error> {
+    writeln!(dest, "{:>indent$}<nav id=\"toc\">", " ")?;
+    writeln!(dest, "{:>indent$}  <h4>{}</h4>", " ", title)?;
+    gen_list(&toc, indent + 2, dest)?;
+    writeln!(dest, "{:>indent$}</nav>", " ")
 }
 
-fn gen_content(dest: &mut File, content: &Vec<Block>) -> Result<(), io::Error> {
-    writeln!(dest, "    <div id=\"content\">")?;
+fn gen_content(dest: &mut File, content: &Vec<Block>, indent: usize) -> Result<(), io::Error> {
+    writeln!(dest, "{:>indent$}<div id=\"content\">", " ")?;
     for block in content {
         match block {
-            Header { spans, level, id } => { gen_header(spans, level, id, dest)?; },
-            Blockquote { spans } => { gen_blockquote(spans, dest)?; },
-            ListElement(list) => { gen_list(list, 6, dest)?; },
-            Table { head, body } => { gen_table(head, body, dest)?; },
-            Paragraph { spans } => { gen_paragraph(spans, dest)?; },
-            CodeBlock { lang, code } => { gen_code_block(lang, code, dest)?; },
+            Header { spans, level, id } => { gen_header(spans, level, id, indent + 2, dest)?; },
+            Blockquote { spans } => { gen_blockquote(spans, indent + 2, dest)?; },
+            ListElement(list) => { gen_list(list, indent + 2, dest)?; },
+            Table { head, body } => { gen_table(head, body, indent + 2, dest)?; },
+            Paragraph { spans } => { gen_paragraph(spans, indent + 2, dest)?; },
+            CodeBlock { lang, code } => { gen_code_block(lang, code, indent + 2, dest)?; },
             _ => {},
         }
     }
-    writeln!(dest, "    </div>")
+    writeln!(dest, "{:>indent$}</div>", " ")
 }
 
-fn gen_header(spans: &Vec<Span>, level: &u32, id: &String, dest: &mut File) -> Result<(), io::Error> {
-    write!(dest, "      <h{} id=\"{}\">", *level, *id)?;
+fn gen_header(spans: &Vec<Span>, level: &u32, id: &String, indent: usize, dest: &mut File) -> Result<(), io::Error> {
+    write!(dest, "{:>indent$}<h{} id=\"{}\">", " ", *level, *id)?;
     gen_spans(spans, dest)?;
     writeln!(dest, "</h{}>", *level)
 }
 
-fn gen_blockquote(spans: &Vec<Span>, dest: &mut File) -> Result<(), io::Error> {
-    write!(dest, "      <blockquote>")?;
+fn gen_blockquote(spans: &Vec<Span>, indent: usize, dest: &mut File) -> Result<(), io::Error> {
+    write!(dest, "{:>indent$}<blockquote>", " ")?;
     gen_spans(spans, dest)?;
     writeln!(dest, "</blockquote>")
 }
@@ -62,54 +62,54 @@ fn gen_list(list: &List, indent: usize, dest: &mut File) -> Result<(), io::Error
         return Ok(());
     }
 
-    writeln!(dest, "{}<{}>", " ".repeat(indent), if list.ordered { "ol" } else { "ul" })?;
+    writeln!(dest, "{:>indent$}<{}>", " ", if list.ordered { "ol" } else { "ul" })?;
     for item in &list.items {
-        writeln!(dest, "{}<li>", " ".repeat(indent + 2))?;
+        writeln!(dest, "{:>indent$}  <li>", " ")?;
         
-        write!(dest, "{}", " ".repeat(indent + 4))?;
+        write!(dest, "{:>indent$}    ", " ")?;
         gen_spans(&item.spans, dest)?;
         writeln!(dest)?;
         gen_list(&item.list, indent + 4, dest)?;
         
-        writeln!(dest, "{}</li>", " ".repeat(indent + 2))?;
+        writeln!(dest, "{:>indent$}  </li>", " ")?;
     }
-    writeln!(dest, "{}</{}>", " ".repeat(indent), if list.ordered { "ol" } else { "ul" })
+    writeln!(dest, "{:>indent$}</{}>", " ", if list.ordered { "ol" } else { "ul" })
 }
 
-fn gen_table(head: &Vec<Vec<String>>, body: &Vec<Vec<String>>, dest: &mut File) -> Result<(), io::Error> {
-    writeln!(dest, "      <table>")?;
+fn gen_table(head: &Vec<Vec<String>>, body: &Vec<Vec<String>>, indent: usize, dest: &mut File) -> Result<(), io::Error> {
+    writeln!(dest, "{:>indent$}<table>", " ")?;
 
-    writeln!(dest, "        <thead>")?;
+    writeln!(dest, "{:>indent$}  <thead>", " ")?;
     for row in head {
-        writeln!(dest, "          <tr>")?;
+        writeln!(dest, "{:>indent$}    <tr>", " ")?;
         for data in row {
-            writeln!(dest, "            <td>{}</td>", *data)?;
+            writeln!(dest, "{:>indent$}      <td>{}</td>", " ", *data)?;
         }
-        writeln!(dest, "          </tr>")?;
+        writeln!(dest, "{:>indent$}    </tr>", " ")?;
     }
-    writeln!(dest, "        </thead>")?;
+    writeln!(dest, "{:>indent$}  </thead>", " ")?;
     
-    writeln!(dest, "        <tbody>")?;
+    writeln!(dest, "{:>indent$}  <tbody>", " ")?;
     for row in body {
-        writeln!(dest, "          <tr>")?;
+        writeln!(dest, "{:>indent$}    <tr>", " ")?;
         for data in row {
-            writeln!(dest, "            <td>{}</td>", *data)?;
+            writeln!(dest, "{:>indent$}      <td>{}</td>", " ", *data)?;
         }
-        writeln!(dest, "          </tr>")?;
+        writeln!(dest, "{:>indent$}    </tr>", " ")?;
     }
-    writeln!(dest, "        </tbody>")?;
+    writeln!(dest, "{:>indent$}  </tbody>", " ")?;
     
-    writeln!(dest, "      </table>")
+    writeln!(dest, "{:>indent$}</table>", " ")
 }
 
-fn gen_code_block(lang: &String, code: &String, dest: &mut File) -> Result<(), io::Error> {
-    write!(dest, "      <pre><code class=\"language-{}\">", if lang == "" { "plaintext" } else { lang })?;
+fn gen_code_block(lang: &String, code: &String, indent: usize, dest: &mut File) -> Result<(), io::Error> {
+    write!(dest, "{:>indent$}<pre><code class=\"language-{}\">", " ", if lang == "" { "plaintext" } else { lang })?;
     write!(dest, "{}", code)?;
     writeln!(dest, "</code></pre>")
 }
 
-fn gen_paragraph(spans: &Vec<Span>, dest: &mut File) -> Result<(), io::Error> {
-    write!(dest, "      <p>")?;
+fn gen_paragraph(spans: &Vec<Span>, indent: usize, dest: &mut File) -> Result<(), io::Error> {
+    write!(dest, "{:>indent$}<p>", " ")?;
     gen_spans(spans, dest)?;
     writeln!(dest, "</p>")
 }
